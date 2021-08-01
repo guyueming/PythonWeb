f991739fb89b0a7d616e094ebc270e58a630293d
顾月明 <guyueming@outlook.com>
2021-08-01 23:43:18 +0800

    木料，桉木皮库存数量显示

--- InventorySystem/InventorySystem/context_processor.py
+++ InventorySystem/InventorySystem/context_processor.py
@@ -6,7 +6,7 @@ def nav_list(request):
     func_list.append(('订单详情', '/home/order/list'))
     func_list.append(('木料', '/home/wood/list'))
     func_list.append(('纸张', '/home/paper/list'))
-    func_list.append(('桉木皮|夹板', '/home/skin/list'))
+    func_list.append(('科技木|桉木皮|夹板', '/home/skin/list'))
     func_list.append(('客户', '/home/customer/list'))
     func_list.append(['销售', '/home/salesman/list'])
     func_list.append(('钢板工艺', '/home/process/technology/list'))
